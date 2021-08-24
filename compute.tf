8fb7e1a19bbddf3b53bc660c6efca5c242f3348a
sudh721 <89455874+sudh721@users.noreply.github.com>
2021-08-24 16:25:48 +0530

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,9 @@
+resource "oci_core_instance" "first_terraform_instance" {
+    # Required
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaagbozcglbeffbh5u3dcrjopxp3yjnfxfaxrfbdyal7qfsskpvspdq"
+    shape = "VM.Standard.E2.1.Micro"
+    source_details {
+        source_id = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaalsm2sac6m57q5uloutt2yyk7ewdsvcywhxigb7gstg4ck4p7pgpq"
+        source_type = "image"
+    }
